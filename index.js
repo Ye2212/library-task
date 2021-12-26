2477d80dccedc9ea7dcf25db1f8412b5e4a7f22c
Yevheniia Ukolova <ukolova_e@ukr.net>
2021-12-26 23:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,130 +39,185 @@ const books = [
 	},
 ];
 
-localStorage.setItem('books', JSON.stringify(books));
 
+if (!localStorage.getItem('books')) {
+    localStorage.setItem('books', JSON.stringify(books));
+}
 const root = document.querySelector('#root');
-
+root.classList.add('container')
 const leftDiv = document.createElement('div');
 const rightDiv = document.createElement('div');
 root.append(leftDiv,rightDiv);
-
 leftDiv.classList.add('leftDiv');
 const h2 = document.createElement('h2');
 h2.textContent = ('');
 const ul = document.createElement('ul');
 const addButton = document.createElement('button');
+addButton.classList.add('btn-warning', 'btn')
 addButton.textContent = 'add';
 leftDiv.append(h2, ul, addButton);
-
 rightDiv.classList.add('rightDiv');
-
-
 const insertUl = document.querySelector('ul')
-
+if (localStorage.getItem('object')) {
+    createBookMarkup(JSON.parse(localStorage.getItem('object')))
+}
 function renderMarkup() {
     const bookMarkup = JSON.parse(localStorage.getItem("books")).map(({ title, id }) => 
-    `<li id= '${id}'><p class ="bookTitle">${title}</p><button class ="edit-element">Edit</button><button class ="delete-element">Delete</button></li>`)
+    `<li id= ${id}><p class ="bookTitle">${title}</p><button class ="edit-element btn btn-primary">Edit</button><button class ="delete-element btn btn-secondary">Delete</button></li>`)
     .join('')
-
     insertUl.insertAdjacentHTML('beforeend', bookMarkup);
     const bookName = document.querySelectorAll('.bookTitle');
     bookName.forEach(element => element.addEventListener('click', renderPreviev));
-
     const buttonEdit = document.querySelectorAll(".edit-element");
     buttonEdit.forEach(button => button.addEventListener('click', onButtonEditClick))
-
     const buttonDelete = document.querySelectorAll(".delete-element");
     buttonDelete.forEach(button => button.addEventListener('click', onButtonDeletetClick))
 }
-
 renderMarkup();
 function renderPreviev(event) {
-
-    const { title, author, img, plot } = JSON.parse(localStorage.getItem("books")).find(element => element.title === event.target.textContent);
-    // console.log(bookToFind);
+    const { title, author, img, plot } = JSON.parse(localStorage.getItem("books")).find(element => element.title === event.currentTarget.textContent);
     function bookToFindMarkup () {
     rightDiv.innerHTML = "";
-        const bookMarkup = `<h2>${title}</h2><p>${author}</p><img src=${img}><p>${plot}</p><button>Save</button>`
+        const bookMarkup = `<h2>${title}</h2><p>${author}</p><img src=${img}><p>${plot}</p>`
         rightDiv.insertAdjacentHTML('beforeend', bookMarkup);
     }
     bookToFindMarkup();
 };
-
-function onButtonEditClick() {
-    console.log("изменяет!") 
+function createBookMarkup(object) {
+    rightDiv.innerHTML = "";
+    const { title, author, img, plot } = object
+    localStorage.setItem('object',JSON.stringify(object))
+    return  rightDiv.insertAdjacentHTML('beforeend', `<h2>${title}</h2><p>${author}</p><img src=${img}><p>${plot}</p>`)
+}
+function onButtonEditClick(event) {
+    const bookToEdit = event.currentTarget.parentNode;
+    
+    console.log("изменяет!")
+    rightDiv.innerHTML = ''
+    
+    const localeStorageData = JSON.parse(localStorage.getItem('books'))
+    console.log(localeStorageData)
+    const editBook = localeStorageData.find(element => {
+        if (element.id === bookToEdit.id) {
+            return element
+        }
+    })
+    rightDiv.insertAdjacentHTML('beforeend', createFormMurkup(editBook))
+    console.log(bookToEdit);
+    console.log(editBook)
+    formFunctionality(editBook)
+    const btnEditSave = document.querySelector('.btn-save')
+    btnEditSave.addEventListener('click', onBtnEditSaveClick)
+    function onBtnEditSaveClick(event) {
+        event.preventDefault();
+        console.log("сохраняет")
+        localStorage.setItem('books', JSON.stringify(localeStorageData))
+        insertUl.innerHTML=''
+        renderMarkup()
+        createBookMarkup(editBook)
+        setTimeout(()=>alert('Books successfully updated'),300)
+    }
 };
 function onButtonDeletetClick(event) {
-const bookToDelete = event.currentTarget.parentNode;
-console.log(bookToDelete);
-const localStorageData = JSON.parse(localStorage.getItem("books"));
-const bookFind = localStorageData.find(element => (element.id === bookToDelete.id)) ;
-console.log(bookFind.title)
-console.log(rightDiv.children[1].textContent)
-if(rightDiv.children[0].textContent === bookFind.title) {
-    rightDiv.innerHTML = '';
-}
-const newData = localStorageData.filter(element => element.id !==bookToDelete.id);
-console.log(newData);
-localStorage.setItem('books',JSON.stringify(newData));
-ul.innerHTML ="";
-renderMarkup(newData);
+    const bookToDelete = event.currentTarget.parentNode;
+    console.log(bookToDelete);
+    const localStorageData = JSON.parse(localStorage.getItem("books"));
+    const bookFind = localStorageData.find(element => {
+        if (element.id === bookToDelete.id) {
+            return element
+        }
+    });
+    const newData = localStorageData.filter(element => {
+        if (element.id !== bookToDelete.id) {
+            return element
+        }
+    });
+    localStorage.setItem('books', JSON.stringify(newData))
+    insertUl.innerHTML = ''
+    renderMarkup()
+    
+    console.log(bookFind)
+    console.log(rightDiv.children[0])
+    
+    if(rightDiv.children[0].textContent === bookFind.title){
+        rightDiv.innerHTML = '';
+    }
 };
-
 addButton.addEventListener('click', onBtnAddClick);
-
-  function createFormMarkup(){
- return `<form><label>Введите название книги<input name ="title"></label>
-    <label>Введите автора книги<input name ="author"></label>
-    <label>Вставьте ссылку на картинку<input name ="img"></label>
-    <label>Введите описание книги<input name ="plot"></label>
-    <button class ='btn-save'>Save</button>
-    </form>`
-  }
-
-
-
-function formFunctionality(book){
-
-const input = document.querySelectorAll('input');
-input.forEach(el => el.addEventListener('change', onInputChange));
-
-
-
-function onInputChange(evt){
-    book[evt.target.name] = evt.target.value;
-    // console.log(book);
-    };
-}
-
-function onBtnAddClick(){
+const inputs = document.querySelectorAll('input')
+function onBtnAddClick() {
+    rightDiv.innerHTML = '';
     const newBook = {
         id: `${Date.now()}`,
-        title: "",
-        author: "",
-        img: "",
-        plot: "",
+        title: '',
+        author: '',
+        img: '',
+        plot:'',
     }
-
-rightDiv.insertAdjacentHTML('beforeend', createFormMarkup());
-formFunctionality(newBook);
-const input = document.querySelectorAll('input');
-const btnSave = document.querySelector('.btn-save');
-
-
-btnSave.addEventListener('click', onBtnSaveClick);
-
-function onBtnSaveClick(evt){
-    evt.preventDefault();
-console.log(newBook);
-input.forEach(el => {
-    if(el.value === "") {
-    alert("Заполните все поля формы!");
+    console.log(newBook)
+    rightDiv.insertAdjacentHTML('beforeend', createFormMurkup(newBook))
+    formFunctionality(newBook)
+    const btnSave = document.querySelector('.btn-save')
+    
+    btnSave.addEventListener('click', onBtnSaveClick);
+    const inputs = document.querySelectorAll('input')
+    
+    function onBtnSaveClick(event) {
+        event.preventDefault();
+        inputs.forEach(input => {
+            if (input.value === '')
+                alert('Заполните все поля')
+            return
+        });
+        if (newBook.title && newBook.author && newBook.img && newBook.plot) {
+            const localStorageData = JSON.parse(localStorage.getItem("books"));
+        const newLS = [...localStorageData, newBook]
+        localStorage.setItem('books', JSON.stringify(newLS))
+        console.log(localStorageData)
+        console.log(newBook)
+        insertUl.innerHTML = ''
+        renderMarkup()
+        createBookMarkup(newBook)
+        setTimeout(() => alert('Book successfully added'),300)
+        }
     }
-})
-localStorage.setItem("form-data", JSON.stringify(newBook))
-};
-};
-
-
-
+}
+function createFormMurkup(book) {
+    //    localStorage.removeItem('object') 
+    
+    
+    return `<form action="">
+        <label for="title">
+        Введите название книги
+            <input name='title' type="text" value="${book.title}"><br>
+        </label>
+        <label for="author">
+        Введите автора книги
+            <input name='author' type="text" value="${book.author}"><br>
+        </label>
+        <label for="img">
+        Вставьте ссылку на картинку
+            <input name='img' type="text" value="${book.img}"><br>
+        </label>
+        <label for="plot">
+        Введите описание книги
+            <input name='plot' type="text" value="${book.plot}"><br>
+        </label>
+        <button class='btn-save' type='submit'>save</button>
+    </form>`
+}
+function formFunctionality(book) {
+    if (book === null) {
+        return
+    }
+    const inputs = document.querySelectorAll('input')
+    inputs.forEach(input => input.addEventListener('change', onInputChange));
+    console.log(inputs)
+    console.log(book)
+    function onInputChange(event) {
+        console.log(event.target)
+        book[event.target.name] = event.target.value
+        console.log(book)
+    }
+   
+}
